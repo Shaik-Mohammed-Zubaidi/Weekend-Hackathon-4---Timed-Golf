5c80083a426ad40a6175277fc9aaf5ff97fa0694
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-11-09 21:41:02 +0530

    dgdghfhgjgj

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,65 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import "../styles/App.css";
-class Timer extends React.Component {
+class Timer extends Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.tid = 0;
+    this.state = { time: 0, x: 0, y: 0, gameStarted: false };
   }
   componentDidMount() {
-    
+    document.getElementById("ball").style = {
+      left: this.state.x + "px",
+      top: this.state.y + "px"
+    };
+  }
+  componentDidUpdate() {
+    if (this.state.x === 250 && this.state.y === 250) {
+      clearInterval(this.tid);
+      document.removeEventListener("keydown", this.moveTheBall);
+    }
   }
 
   componentWillUnmount() {
-    
+    clearInterval(this.tid);
   }
-
-
+  moveTheBall = (event) => {
+    if (event.key === "ArrowDown" || event.keyCode === 40) {
+      this.setState({ y: this.state.y + 5 });
+    } else if (event.key === "ArrowUp" || event.keyCode === 38) {
+      this.setState({ y: this.state.y - 5 });
+    } else if (event.key === "ArrowRight" || event.keyCode === 39) {
+      this.setState({ x: this.state.x + 5 });
+    } else if (event.key === "ArrowLeft" || event.keyCode === 37) {
+      this.setState({ x: this.state.x - 5 });
+    }
+  };
+  startGame = () => {
+    this.tid = setInterval(() => {
+      let t = this.state.time;
+      this.setState({ time: t + 1 });
+    }, 1000);
+    this.setState({ gameStarted: true });
+    document.addEventListener("keydown", this.moveTheBall, false);
+  };
 
   render() {
     return (
- <>
-</>
+      <>
+        <div
+          className="ball"
+          id="ball"
+          style={{ left: this.state.x + "px", top: this.state.y + "px" }}
+        ></div>
+        <div className="hole"></div>
+        <div className="heading-timer">{this.state.time}</div>
+        <button
+          className="start"
+          onClick={this.startGame}
+          disabled={this.state.gameStarted}
+        >
+          Start
+        </button>
+      </>
     );
   }
 }
