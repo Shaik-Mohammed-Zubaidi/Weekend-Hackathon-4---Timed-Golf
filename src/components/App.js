4e123df3b36dc6b64d6c1501adade08c14aa7768
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-11-12 01:04:00 +0530

    jhjbhbj

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,13 @@ class Timer extends Component {
   constructor(props) {
     super(props);
     this.tid = 0;
-    this.state = { time: 0, x: 0, y: 0 };
+    this.state = { time: 0, x: 0, y: 0, isStarted: false };
     this.moveTheBall = this.moveTheBall.bind(this);
     this.startGame = this.startGame.bind(this);
   }
+
   componentDidMount() {
-    document.getElementsByClassName("ball")[0].style = {
-      left: this.state.x + "px",
-      top: this.state.y + "px"
-    };
+    document.addEventListener("keydown", this.moveTheBall, false);
   }
   componentDidUpdate() {
     if (this.state.x === 250 && this.state.y === 250) {
@@ -20,40 +18,46 @@ class Timer extends Component {
       document.removeEventListener("keydown", this.moveTheBall);
     }
   }
-
   componentWillUnmount() {
+    document.removeEventListener("keydown", this.moveTheBall);
     clearInterval(this.tid);
   }
+
   moveTheBall(event) {
+    let { x, y } = this.state;
+    if (!this.state.isStarted) {
+      return;
+    }
     if (event.key === "ArrowDown" || event.keyCode === 40) {
-      this.setState({ y: this.state.y + 5 });
+      this.setState({ y: y + 5 });
     } else if (event.key === "ArrowUp" || event.keyCode === 38) {
-      this.setState({ y: this.state.y - 5 });
+      this.setState({ y: y - 5 });
     } else if (event.key === "ArrowRight" || event.keyCode === 39) {
-      this.setState({ x: this.state.x + 5 });
+      this.setState({ x: x + 5 });
     } else if (event.key === "ArrowLeft" || event.keyCode === 37) {
-      this.setState({ x: this.state.x - 5 });
+      this.setState({ x: x - 5 });
     }
   }
+
   startGame() {
+    this.setState({ isStarted: true });
     this.tid = setInterval(() => {
       let t = this.state.time;
       this.setState({ time: t + 1 });
     }, 1000);
-    document.addEventListener("keydown", this.moveTheBall, false);
   }
 
   render() {
     let ballPosition = { left: this.state.x + "px", top: this.state.y + "px" };
     return (
-      <div>
+      <>
         <div className="ball" style={ballPosition}></div>
-        <div className="hole"></div>
-        <div className="heading-timer">{this.state.time}</div>
         <button className="start" onClick={this.startGame}>
           Start
         </button>
-      </div>
+        <div className="hole"></div>
+        <div className="heading-timer">{this.state.time}</div>
+      </>
     );
   }
 }
