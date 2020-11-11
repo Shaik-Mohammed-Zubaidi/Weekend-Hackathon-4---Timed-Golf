e1f58be09175a9be4859a23123ddd5003bc23120
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-11-11 23:39:01 +0530

    vnngjgj

--- src/components/App.js
+++ src/components/App.js
@@ -1,62 +1,98 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "../styles/App.css";
-class Timer extends Component {
+class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.tid = 0;
-    this.state = { time: 0, x: 0, y: 0 };
-    this.moveTheBall = this.moveTheBall.bind(this);
-    this.startGame = this.startGame.bind(this);
+    this.state = { time: 0, x: 0, y: 0 ,isStarted:false};
+    this.handleArrowClick = this.handleArrowClick.bind(this);
+    this.handleStartButtonClick = this.handleStartButtonClick.bind(this);
+    this.startTimer = this.startTimer.bind(this);
   }
   componentDidMount() {
-    document
-      .getElementsByClassName("start")[0]
-      .addEventListener("click", this.startGame);
-    document.getElementsByClassName("ball")[0].style = {
-      left: this.state.x + "px",
-      top: this.state.y + "px"
-    };
-  }
-  componentDidUpdate() {
-    if (this.state.x === 250 && this.state.y === 250) {
-      clearInterval(this.tid);
-      document.removeEventListener("keydown", this.moveTheBall);
-    }
+    document.addEventListener("keydown", this.handleArrowClick, false);
   }
 
   componentWillUnmount() {
-    clearInterval(this.tid);
+    document.removeEventListener("keydown", this.handleArrowClick, false);
+    clearInterval(this.interval);
   }
-  moveTheBall(event) {
-    if (event.key === "ArrowDown" || event.keyCode === 40) {
-      this.setState({ y: this.state.y + 5 });
-    } else if (event.key === "ArrowUp" || event.keyCode === 38) {
-      this.setState({ y: this.state.y - 5 });
-    } else if (event.key === "ArrowRight" || event.keyCode === 39) {
-      this.setState({ x: this.state.x + 5 });
-    } else if (event.key === "ArrowLeft" || event.keyCode === 37) {
-      this.setState({ x: this.state.x - 5 });
-    }
-  }
-  startGame() {
-    this.tid = setInterval(() => {
-      let t = this.state.time;
-      this.setState({ time: t + 1 });
+  
+  startTimer(){
+    this.interval = setInterval(() => {
+      this.setState(prevState => ({
+        time: prevState.time + 1
+      }));
     }, 1000);
-    document.addEventListener("keydown", this.moveTheBall, false);
+   }
+  
+
+  handleArrowClick(event) {
+    let {x,y} = this.state;
+    if(!this.state.isStarted){
+      return;
+    }
+    if(x === 250 && y === 250){
+      clearInterval(this.interval);
+      return;
+    }
+    if (event.keyCode === 39) {
+      let nx = Number(x + 5);
+      let ballPosition = {
+        left: nx,
+        top: y
+      };
+      this.setState({x:ballPosition.left,y:ballPosition.top});
+    } 
+    else if (event.keyCode === 40) {
+      let ny = Number(y + 5);
+      let ballPosition = {
+        left:x,
+        top: ny
+      };
+      this.setState({x:ballPosition.left,y:ballPosition.top});
+
+    } else if (event.keyCode === 37) {
+      let nx = Number(x - 5);
+      let ballPosition = {
+        left: nx,
+        top: y
+      };
+      this.setState({x:ballPosition.left,y:ballPosition.top});
+    } else if (event.keyCode === 38) {
+      let ny = Number(y - 5);
+      let ballPosition = {
+        left:x,
+        top: ny
+      };
+      this.setState({x:ballPosition.left,y:ballPosition.top});
+    
+   }
   }
 
+  handleStartButtonClick(){
+    if(!this.state.isStarted){
+      this.setState({isStarted:true},()=>{
+        this.startTimer();
+      })
+    }
+  }
+
+
+
   render() {
+    let ballPosition = {
+        left:`${this.state.x}px`,
+        top:`${this.state.y}px`,
+    }
     return (
-      <div>
-        <div
-          className="ball"
-          style={{ left: this.state.x + "px", top: this.state.y + "px" }}
-        ></div>
-        <div className="hole"></div>
-        <div className="heading-timer">{this.state.time}</div>
-        <button className="start">Start</button>
-      </div>
+            <>
+            <div className="ball" style={ballPosition}></div>;
+            <button className="start" onClick={this.handleStartButtonClick}>
+                 Start
+           </button>
+           <div className="heading-timer">{this.state.time}</div>
+           <div className="hole"></div>
+            </>
     );
   }
 }
